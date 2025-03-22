c2d759ee723dd8347569bb874b04a39b4e343f5d
Anjan Debnath <anjan0151@gmail.com>
2025-03-22 14:53:00 +0600

    rabbitmq is used to async communication among the microservices

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,91 +1,68 @@
 const express = require("express");
 const fs = require("fs");
-const http = require("http");
+const amqp = require('amqplib');
 
-//
-// Send the "viewed" to the history microservice.
-//
-function sendViewedMessage(videoPath) {
-    const postOptions = { // Options to the HTTP POST request.
-        method: "POST", // Sets the request method as POST.
-        headers: {
-            "Content-Type": "application/json", // Sets the content type for the request's body.
-        },
-    };
-
-    const requestBody = { // Body of the HTTP POST request.
-        videoPath: videoPath 
-    };
-
-    const req = http.request( // Send the "viewed" message to the history microservice.
-        "http://video-history/viewed",
-        postOptions
-    );
-
-    req.on("close", () => {
-        console.log("Sent 'viewed' message to history microservice.");
-    });
-
-    req.on("error", (err) => {
-        console.error("Failed to send 'viewed' message!");
-        console.error(err && err.stack || err);
-    });
-
-    req.write(JSON.stringify(requestBody)); // Write the body to the request.
-    req.end(); // End the request.
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
-//
-// Setup event handlers.
-//
-function setupHandlers(app) {
-    app.get("/video", (req, res) => { // Route for streaming video.
-
-        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
-        fs.stat(videoPath, (err, stats) => {
-            if (err) {
-                console.error("An error occurred ");
-                res.sendStatus(500);
-                return;
-            }
-    
-            res.writeHead(200, {
-                "Content-Length": stats.size,
-                "Content-Type": "video/mp4",
-            });
-    
-            fs.createReadStream(videoPath).pipe(res);
-
-            sendViewedMessage(videoPath); // Send message to "history" microservice that this video has been "viewed".
-        });
-    });
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
 }
 
-//
-// Start the HTTP server.
-//
-function startHttpServer() {
-    return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
-        const app = express();
-        setupHandlers(app);
-        
-        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
-        app.listen(port, () => {
-            resolve();
-        });
-    });
-}
+const PORT = process.env.PORT;
+const RABBIT = process.env.RABBIT;
 
+
+  
 //
 // Application entry point.
 //
-function main() {
-    return startHttpServer();
+async function main() {
+
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+    const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
+
+    console.log("Connected to RabbitMQ.");
+
+    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
+    
+	//
+	// Send the "viewed" to the history microservice.
+	//
+	function sendViewedMessage(messageChannel, videoPath) {
+	    console.log(`Publishing message on "viewed" queue.`);
+	
+	    const msg = { videoPath: videoPath };
+	    const jsonMsg = JSON.stringify(msg);
+	    messageChannel.publish("", "viewed", Buffer.from(jsonMsg)); // Publishes message to the "viewed" queue.
+	}
+
+    const app = express();
+
+    app.get("/video", async (req, res) => { // Route for streaming video.
+
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        const stats = await fs.promises.stat(videoPath);
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+    
+        fs.createReadStream(videoPath).pipe(res);
+
+        sendViewedMessage(messageChannel, videoPath); // Sends the "viewed" message to indicate this video has been watched.
+    });
+
+    app.listen(PORT, () => {
+        console.log("Streaming Microservice online.");
+    });
 }
 
 main()
-    .then(() => console.log("Streaming Microservice online."))
     .catch(err => {
-        console.error("Microservice failed to start.");
+        console.error("Streaming Microservice failed to start.");
         console.error(err && err.stack || err);
     });
